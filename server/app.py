1b8943d000c0bc8bc406a54e3c15b927b5f935d2
Gilbert Hawkins Winja <104626373+hawkinswinja@users.noreply.github.com>
2024-05-27 18:43:18 +0000

    feat: api rate limiting at 10/minute using Flask Limiter implemented rate limiting with redis for backed memory and added a custom error handler implemented compose file for the same to spin up the services utilizing health checks

--- server/app.py
+++ server/app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
 from .predictor import handle_prediction
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from os import getenv
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+limiter = Limiter(
+  get_remote_address,
+  app=app,
+  storage_uri=getenv('REDIS'),
+  storage_options={"socket_connect_timeout": 30},
+  default_limits=["100 per day", "10 per minute"],
+  strategy="fixed-window", # or "moving-window"
+)
+
+
+@app.errorhandler(429)
+def ratelimit_handler(e):
+    return jsonify(error=f"ratelimit exceeded {e.description}"), 429
 
 @app.route('/')
 def home():
@@ -11,7 +28,6 @@ def home():
 
 @app.route('/status')
 def status():
-    # Example data, replace with real data or logic as needed
     return {'status': 'API is running'}
 
 
